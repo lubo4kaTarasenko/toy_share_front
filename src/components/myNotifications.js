c63a423d05def59e7027e88e27697b87a0fc4a29
luba <lubo4ka.boiko@gmail.com>
2021-04-26 11:53:08 +0300

    notifications date

--- src/components/myNotifications.js
+++ src/components/myNotifications.js
@@ -16,13 +16,20 @@ export default function MyNotifications() {
       (result) => {
         setNotifications(result.notifications)          
       })
+    new NotificationsApi().updateAllNotifications().then(
+      (result) => {
+        console.log('yep')        
+    })
   }
   
   function renderList(){
     return(
       notifications.map(notification => {
         return (
-          <div key={notification.id} className={notification.status == 'new' ? 'notification new_notification' : 'notification viewd_notification' }>{notification.body}</div>
+          <div key={notification.id} className={notification.status == 'new' ? 'notification new_notification' : 'notification viewd_notification' }>
+            {notification.body}
+            <span className='notification_date'>{notification.created_date}</span>
+          </div>
         )
       })
     )
